13d4e2721bc49edd1498b8c78ecbac8ec665ed6b
0429oonishi <oonishireon110@gmail.com>
2021-08-03 08:02:13 +0900

    DataStore作成

--- RxSwift-Tax-Practice/Model/TaxUserDefaultsDataStore.swift
+++ RxSwift-Tax-Practice/Model/TaxUserDefaultsDataStore.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+protocol TaxDataStoreProtocol: AnyObject {
+    func save(consumptionTax: Int)
+    func loadConsumptionTax() -> Int
+}
+
+final class TaxUserDefaultsDataStore: TaxDataStoreProtocol {
+    
+    private let saveKey = "saveKey"
+    
+    func save(consumptionTax: Int) {
+        UserDefaults.standard.set(consumptionTax, forKey: saveKey)
+    }
+    
+    func loadConsumptionTax() -> Int {
+        return UserDefaults.standard.integer(forKey: saveKey)
+    }
+    
+}
