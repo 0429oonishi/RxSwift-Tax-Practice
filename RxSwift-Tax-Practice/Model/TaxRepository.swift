07827d6296defdc805eb8ad9a5bb490380786058
0429oonishi <oonishireon110@gmail.com>
2021-08-03 08:01:56 +0900

    TaxRepository作成

--- RxSwift-Tax-Practice/Model/TaxRepository.swift
+++ RxSwift-Tax-Practice/Model/TaxRepository.swift
@@ -5,4 +5,28 @@
 //  Created by 大西玲音 on 2021/08/03.
 //
 
-import Foundation
+import RxSwift
+
+protocol TaxRepositoryProtocol: AnyObject {
+    func save(consumptionTax: Int) -> Completable
+    func loadConsumptionTax() -> Single<Int>
+}
+
+final class TaxRepository: TaxRepositoryProtocol {
+    
+    private let dataStore: TaxDataStoreProtocol
+    
+    init(dataStore: TaxDataStoreProtocol) {
+        self.dataStore = dataStore
+    }
+    
+    func save(consumptionTax: Int) -> Completable {
+        dataStore.save(consumptionTax: consumptionTax)
+        return Completable.empty()
+    }
+    
+    func loadConsumptionTax() -> Single<Int> {
+        return .just(dataStore.loadConsumptionTax())
+    }
+    
+}
