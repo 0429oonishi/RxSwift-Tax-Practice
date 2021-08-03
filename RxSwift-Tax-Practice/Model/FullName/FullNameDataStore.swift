d2062559229576daec28ac7974e5e1915515bab7
0429oonishi <oonishireon110@gmail.com>
2021-08-03 10:57:42 +0900

    FullNameファイル作成

---
+++ RxSwift-Tax-Practice/Model/FullName/FullNameDataStore.swift
@@ -0,0 +1,8 @@
+//
+//  FullNameDataStore.swift
+//  RxSwift-Tax-Practice
+//
+//  Created by 大西玲音 on 2021/08/03.
+//
+
+import Foundation
---
+++ RxSwift-Tax-Practice/View/Controller/FullNameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FullNameViewController.swift
+//  RxSwift-Tax-Practice
+//
+//  Created by 大西玲音 on 2021/08/03.
+//
+
+import UIKit
+
+class FullNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
